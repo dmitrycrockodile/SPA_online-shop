54d1ffb3ddb2422abc89c480eb7f72236268717d
Dmitrij <dmitry.bermas@gmail.com>
2022-09-19 12:30:30 +0200

    removed unused Link import

--- src/components/pages/productPage/ProductPage.jsx
+++ src/components/pages/productPage/ProductPage.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { connect } from 'react-redux';
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { Interweave } from 'interweave';
 
